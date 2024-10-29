2640942fe325cdd1762a67d1bf7159b1b1ca51fc
shivadns <145219091+shivadns88@users.noreply.github.com>
2024-10-29 19:30:47 +0000

    Adding S3 backend for Terraform

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,9 @@
-terraform {}
+terraform {
+  required_version = "~> 1.9.5"
+  backend "s3" {
+    bucket         = "shivadns-gitops-terraform-backend"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "shivadns-GitopsTerraformLocks"
+  }
+}
